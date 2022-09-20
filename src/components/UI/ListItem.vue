7b0e1d1682466a3f7b1e1765f17140190ffc7d2e
notarious <notarious2@gmail.com>
2022-09-20 17:43:09 +0500

    added about page draft - with list rendering (v-for) of same component with multiple params

---
+++ src/components/UI/ListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <li @click="toggleDesc" :class="{ selectedList: isSelected }">
+      {{ skill }}
+    </li>
+    <span class="desc" v-if="showDesc">{{ skillDesc }}</span>
+  </div>
+</template>
+<script setup>
+import { ref, defineProps } from "vue";
+
+// eslint-disable-next-line
+const props = defineProps(["skill", "skillDesc"]);
+
+const showDesc = ref(false);
+const isSelected = ref(false);
+
+function toggleDesc() {
+  showDesc.value = !showDesc.value;
+  isSelected.value = !isSelected.value;
+}
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  list-style: none;
+  box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px;
+  padding: 10px;
+  margin-top: 10px;
+  margin-bottom: 5px;
+  align-items: center;
+  user-select: none;
+}
+.desc {
+  margin-top: 5px;
+  margin-left: 20px;
+  padding: 5px;
+  font-size: 16px;
+}
+
+.selectedList {
+  background-color: aquamarine;
+}
+</style>
--- src/pages/TheAbout.vue
+++ src/pages/TheAbout.vue
@@ -1,5 +1,49 @@
 <template>
-  <base-card>
-    <h1>The About Page</h1>
-  </base-card>
+  <div>
+    <h3>
+      I have utilized the following Vue concepts when implementing this project:
+    </h3>
+
+    <component
+      :is="ListItem"
+      :skill="set.skill"
+      :skill-desc="set.description"
+      v-for="set in skillsets"
+      :key="set.skill"
+    />
+  </div>
 </template>
+
+<script setup>
+import ListItem from "../components/UI/ListItem.vue";
+
+// eslint-disable-next-line
+const skillsets = [
+  {
+    skill: "Composition API",
+    description:
+      "I first used Options API and then switched to Composition API",
+  },
+  { skill: "Input", description: "add Description here" },
+  { skill: "Page transition", description: "add Description here" },
+  { skill: "Button Animation", description: "add Description here" },
+  {
+    skill: "Vue router for page navigation and styling active pages",
+    description: "add Description here",
+  },
+  {
+    skill: "Component interaction with props",
+    description: "add Description here",
+  },
+  {
+    skill: "List Rendering v-for with a Component",
+    description: "this page for example was created this way",
+  },
+];
+</script>
+
+<style scoped>
+h3 {
+  text-align: center;
+}
+</style>
