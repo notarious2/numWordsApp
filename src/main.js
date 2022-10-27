c8ffec19a71b9861860112b41e796ee8229cea61
notarious1~git config --global user.email notarious2@gmail.comgit config --global user.name notarious <notarious2@gmail.com>
2022-10-27 12:22:28 +0500

    added vue-gtag

--- src/main.js
+++ src/main.js
@@ -16,6 +16,8 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 
+import VueGtag from "vue-gtag";
+
 library.add(fab, fas, far);
 
 const routes = [
@@ -53,6 +55,7 @@ const router = createRouter({
 const app = createApp(App);
 
 app.use(router);
+app.use(VueGtag, { config: { id: "G-BVPH5TSYNW" } }); // for google analytics
 app.component("NavigationBar", TheNavigationBar);
 app.component("TheFooter", TheFooter);
 app.component("base-card", BaseCard);
