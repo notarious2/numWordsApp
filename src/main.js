a725d7b1beb5eeeccc5c482a483550a7e541e3b7
notarious1~git config --global user.email notarious2@gmail.comgit config --global user.name notarious <notarious2@gmail.com>
2022-11-07 11:45:02 +0500

    moved router to separate file and change icon

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 
 import App from "./App.vue";
 import TheNavigationBar from "./components/UI/TheNavigationBar.vue";
@@ -12,45 +11,11 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 
+import router from "./router.js";
+
 import VueGtag from "vue-gtag";
 
-import TheInput from "./components/TheInput.vue";
-import TheAbout from "./pages/about/TheAbout.vue";
-import TheContact from "./pages/TheContact.vue";
-import NotFound from "./pages/NotFound.vue";
-
-const routes = [
-  // {
-  //   path: "/",
-  //   redirect: { name: "Home" },
-  // },
-  {
-    // path: "/numWordsApp/",
-    path: "/",
-    name: "Home",
-    component: TheInput,
-  },
-  {
-    // path: "/numWordsApp/about",
-    path: "/about",
-    name: "About",
-    component: TheAbout,
-  },
-  {
-    // path: "/numWordsApp/contact",
-    path: "/contact",
-    name: "Contact",
-    component: TheContact,
-  },
-  { path: "/:notFound(.*)", component: NotFound },
-];
 library.add(fab, fas, far);
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-  linkExactActiveClass: "active", // to highlight active tabs
-});
 
 const app = createApp(App);
 
---
+++ src/router.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const TheInput = () => import("./components/TheInput.vue");
+const TheAbout = () => import("./pages/about/TheAbout.vue");
+const TheContact = () => import("./pages/TheContact.vue");
+const NotFound = () => import("./pages/NotFound.vue");
+
+const routes = [
+  // {
+  //   path: "/",
+  //   redirect: { name: "Home" },
+  // },
+  {
+    // path: "/numWordsApp/",
+    path: "/",
+    name: "Home",
+    component: TheInput,
+  },
+  {
+    // path: "/numWordsApp/about",
+    path: "/about",
+    name: "About",
+    component: TheAbout,
+  },
+  {
+    // path: "/numWordsApp/contact",
+    path: "/contact",
+    name: "Contact",
+    component: TheContact,
+  },
+  { path: "/:notFound(.*)", component: NotFound },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  linkExactActiveClass: "active", // to highlight active tabs
+});
+
+export default router;
